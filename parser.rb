0ba0b6907b87e7d04495ae0a9f87935a5c2e0e00
Damir Svrtan <damir.svrtan@gmail.com>
2014-12-02 21:54:26 +0100

    Initial parser finished.

---
+++ parser.rb
@@ -0,0 +1,62 @@
+require 'pry'
+require 'open-uri'
+require 'nokogiri'
+require 'yaml'
+require 'active_support/core_ext/string'
+
+THEMES_URL = "https://github.com/imathis/octopress/wiki/3rd-Party-Octopress-Themes"
+
+doc = Nokogiri::HTML(open(THEMES_URL))
+
+Dir.mkdir('_posts') unless File.directory?('_posts')
+
+doc.xpath("//table//tbody//tr").each do |tr|
+  tds = tr.xpath("./td")
+
+  first_td  = tds[0]
+  second_td = tds[1]
+  third_td  = tds[2]
+
+  # Hash for prefilling the front matter YAML for jekyll.
+  post = {}
+
+  post[:title] = first_td.xpath("./a")[0].text
+
+  # encoding fixes
+  if post[:title] =~ /nigspress/
+    post[:title] = "Konigspress"
+  end
+
+  post[:github_link] = first_td.xpath("./a")[0].attributes['href'].value
+
+  if second_td.xpath("./a").count == 2
+
+  else
+    link_text = second_td.xpath('./a').text
+
+    if link_text == 'Preview'
+      post[:demo_preview] = second_td.xpath('./a')[0].attributes['href'].value
+      post[:demo_screenshot] = nil
+    elsif link_text == 'Screenshot' || link_text == 'Screenshots'
+      post[:demo_screenshot] = second_td.xpath('./a')[0].attributes['href'].value
+      post[:demo_preview] = nil
+    else
+      binding.pry
+    end
+  end
+
+  post[:description] = third_td.text
+
+  File.open("_posts/2014-12-02-#{post[:title].underscore.dasherize}.md", 'w') do |f|
+    YAML.dump(post, f)
+    f.write("---")
+  end
+
+end
+
+# def write_to_console(post)
+#   post.each do |k, v|
+#     puts "#{k}: #{v}"
+#   end
+#   puts "---------"
+# end
